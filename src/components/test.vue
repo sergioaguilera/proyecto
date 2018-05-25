a13d53b5ee60b6167c6961a9c5026da45d391495
Sergio Aguilera <sergio.aaguilera@gmail.com>
2018-05-25 12:56:19 -0500

    Initial commit

---
+++ src/components/test.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+<div v-html="myhtml"></div>
+    <div v-if="showname">{{user.firstname}} </div>
+    <div v-else="showname">No te conozco carnal</div>
+    <hr>
+    
+    <ul>
+        <li v-for="task in tasks">
+    {{task.title}}
+    </li>
+    
+    </ul>
+    
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            title:"Mi primer app con vue.js",
+            user:{
+                firstname:'sergio',
+                lastname: 'aguilera'
+            },
+            myhtml:'<h3>Hola Vue</h3>',
+            showname:false,
+            tasks:[
+                
+                {title:'MTG'},
+                {title:'DM'},
+                {title:'DND'},
+            ]
+        }
+    }
+    
+}
+</script>
+
+<style media="screen">
+
+</style>
